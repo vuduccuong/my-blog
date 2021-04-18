5331b4f484fe6ec3477c4633cd49703e579d9294
vuduccuong <vuduccuong.ck48@gmail.com>
2021-04-18 12:03:26 +0700

    audio

--- components/quote/quote.js
+++ components/quote/quote.js
@@ -1,16 +1,25 @@
 import styles from "./quote.module.css";
 
 const Quote = () => {
+  const PlayHHH = () => {
+    const audioEl = document.getElementsByClassName("audio-element")[0];
+    audioEl.play();
+  };
+
   return (
     <div className={styles.main}>
       <div className={styles.quote__container}>
         <div className={styles.quote__text}>
           <i className="fas" className={styles.fa_quote_left}></i>
-          <span id="quote" className={styles.long__quote}>
-            Xã hội này chỉ có làm, chịu khó, cần cù thì bù siêng năng, chỉ có làm
-            thì mới có ăn, những cái loại không làm mà đòi có ăn thì ăn đầu buồi
-            ăn cứt!
+          <span id="quote" className={styles.long__quote} onClick={PlayHHH}>
+            Xã hội này chỉ có làm, chịu khó, cần cù thì bù siêng năng, chỉ có
+            làm thì mới có ăn, những cái loại không làm mà đòi có ăn thì ăn đầu
+            b ăn c!
           </span>
+
+          <audio className="audio-element">
+            <source src="./voices/Huan_Hoa_Hong.mp3"></source>
+          </audio>
         </div>
 
         <div className={styles.quote__author}>
